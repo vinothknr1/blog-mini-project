5fd518d7bf38f44ff3c9880390b1d60e063013bc
VinothKumar <89235649+vinowoualabs@users.noreply.github.com>
2022-04-01 23:02:56 +0530

    Server Setup Completed

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+//Imports
+import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import {} from 'dotenv/config';
+
+//app
+const app = express();
+
+// middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+//cors
+app.use(cors());
+
+//routes
+app.get('/api', (req, res) => {
+  res.json({ time: Date().toString() });
+});
+
+//port
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
