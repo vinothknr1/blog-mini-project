fbea15abfd71e417900dfc46ea9257f33b3b8eb4
VinothKumar <89235649+vinowoualabs@users.noreply.github.com>
2022-04-05 18:42:07 +0530

    Signup Validator Completed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,7 +3,11 @@ import express from 'express';
 const router = express.Router();
 import { signup } from '../controllers/auth.js';
 
+//validators
+import { runValidation } from '../validators/index.js';
+import { userSignupValidator } from '../validators/auth.js';
+
 //Handling Routes
-router.post('/signup', signup);
+router.post('/signup', userSignupValidator, runValidation, signup);
 
 export default router;
