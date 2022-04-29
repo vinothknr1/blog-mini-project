06c26790f2d8fa30bbeca2129542ae14db3232ed
aamec <aamec@CSESER.EDU>
2022-04-29 15:58:15 +0600

    Read and Delete Blog with an issue 82

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -1,6 +1,6 @@
 //Import
 import express from 'express';
-import { create } from '../controllers/blog.js';
+import { create, list, listAllBlogsCategoriesTags, read, remove, update } from '../controllers/blog.js';
 import { requireSignin, adminMiddleware } from '../controllers/auth.js';
 
 //Declaration
@@ -8,5 +8,11 @@ const router = express.Router();
 
 //Handling Routes
 router.post('/blog', requireSignin, adminMiddleware, create);
+router.get('/blogs', list);
+router.post('/blogs-categories-tags', listAllBlogsCategoriesTags);
+router.get('/blog/:slug', read);
+router.delete('/blog/:slug', requireSignin, adminMiddleware, remove);
+router.put('/blog/:slug', requireSignin, adminMiddleware, update);
+
 
 export default router;
