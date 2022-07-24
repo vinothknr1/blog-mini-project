86fcac216e88676106cb8f82304647d0253f461b
VinothKumar <89235649+vinowoualabs@users.noreply.github.com>
2022-07-24 07:44:47 +0530

    Blog Update and Show Blog Photo Completed with photo retrive problem in postman

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -1,18 +1,26 @@
 //Import
-import express from 'express';
-import { create, list, listAllBlogsCategoriesTags, read, remove, update } from '../controllers/blog.js';
-import { requireSignin, adminMiddleware } from '../controllers/auth.js';
+import express from "express";
+import {
+  create,
+  list,
+  listAllBlogsCategoriesTags,
+  read,
+  remove,
+  update,
+  photo,
+} from "../controllers/blog.js";
+import { requireSignin, adminMiddleware } from "../controllers/auth.js";
 
 //Declaration
 const router = express.Router();
 
 //Handling Routes
-router.post('/blog', requireSignin, adminMiddleware, create);
-router.get('/blogs', list);
-router.post('/blogs-categories-tags', listAllBlogsCategoriesTags);
-router.get('/blog/:slug', read);
-router.delete('/blog/:slug', requireSignin, adminMiddleware, remove);
-router.put('/blog/:slug', requireSignin, adminMiddleware, update);
-
+router.post("/blog", requireSignin, adminMiddleware, create);
+router.get("/blogs", list);
+router.post("/blogs-categories-tags", listAllBlogsCategoriesTags);
+router.get("/blog/:slug", read);
+router.delete("/blog/:slug", requireSignin, adminMiddleware, remove);
+router.put("/blog/:slug", requireSignin, adminMiddleware, update);
+router.get("/blog/photo/:slug", photo);
 
 export default router;
