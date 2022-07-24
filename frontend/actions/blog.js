99a7e4f169065ba015c1c8ce2df64dfb30437996
VinothKumar <89235649+vinowoualabs@users.noreply.github.com>
2022-07-24 09:07:46 +0530

    Show Completed with Updated Time issue

--- frontend/actions/blog.js
+++ frontend/actions/blog.js
@@ -1,14 +1,27 @@
-import fetch from 'isomorphic-fetch';
-import { API } from '../config';
+import fetch from "isomorphic-fetch";
+import { API } from "../config";
 
 export const createBlog = (blog, token) => {
   return fetch(`http://localhost:8000/api/blog`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      Accept: 'application/json',
+      Accept: "application/json",
       Authorization: `Bearer ${token}`,
     },
     body: blog,
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+export const listBlogsWithCategoriesAndTags = () => {
+  return fetch(`http://localhost:8000/api/blogs-categories-tags`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+    },
   })
     .then((response) => {
       return response.json();
